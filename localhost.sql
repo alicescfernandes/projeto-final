4219f49dfd92ed71d82ea1470ff54c25ed9f7f02
Alice <alicescfernandes@gmail.com>
2017-02-16 22:53:59 +0000

    added new sql

--- localhost.sql
+++ localhost.sql
@@ -2,10 +2,10 @@
 -- version 4.0.4.2
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Feb 14, 2017 at 12:10 PM
--- Server version: 5.6.13
--- PHP Version: 5.4.17
+-- Máquina: localhost
+-- Data de Criação: 16-Fev-2017 às 22:53
+-- Versão do servidor: 5.6.13
+-- versão do PHP: 5.4.17
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,15 +17,15 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `id802211_portfolio`
+-- Base de Dados: `id802211_portfolio`
 --
-CREATE DATABASE IF NOT EXISTS `id802211_portfolio` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
+CREATE DATABASE IF NOT EXISTS `id802211_portfolio` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 USE `id802211_portfolio`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `external-urls`
+-- Estrutura da tabela `external-urls`
 --
 
 CREATE TABLE IF NOT EXISTS `external-urls` (
@@ -38,7 +38,7 @@ CREATE TABLE IF NOT EXISTS `external-urls` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3 ;
 
 --
--- Dumping data for table `external-urls`
+-- Extraindo dados da tabela `external-urls`
 --
 
 INSERT INTO `external-urls` (`projeto_uri`, `id`, `github`, `codepen`, `industria-criativa`) VALUES
@@ -48,7 +48,7 @@ INSERT INTO `external-urls` (`projeto_uri`, `id`, `github`, `codepen`, `industri
 -- --------------------------------------------------------
 
 --
--- Table structure for table `media`
+-- Estrutura da tabela `media`
 --
 
 CREATE TABLE IF NOT EXISTS `media` (
@@ -59,47 +59,69 @@ CREATE TABLE IF NOT EXISTS `media` (
   `ext` varchar(50) COLLATE utf8_bin NOT NULL,
   `cover` tinyint(1) DEFAULT NULL,
   `title` varchar(1000) COLLATE utf8_bin NOT NULL,
+  `nome` varchar(100) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id_media`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=22 ;
 
 --
--- Dumping data for table `media`
+-- Extraindo dados da tabela `media`
 --
 
-INSERT INTO `media` (`id_media`, `projeto_uri`, `tipo`, `src`, `ext`, `cover`, `title`) VALUES
-(1, 'dummy-project', 'image', 'img', 'jpg', 1, 'Design Gr&#xE1;fico'),
-(2, 'dummy-project', 'image', 'img1', 'jpg', NULL, 'fotografia'),
-(3, 'dummy-project2', 'image', 'img2', 'jpg', NULL, 'web development'),
-(4, 'dummy-project2', 'image', 'img3', 'jpg', NULL, 'fotografia'),
-(5, 'dummy-project2', 'image', 'img4', 'jpg', NULL, 'fotografia');
+INSERT INTO `media` (`id_media`, `projeto_uri`, `tipo`, `src`, `ext`, `cover`, `title`, `nome`) VALUES
+(1, 'fotografia', 'image', 'img', 'jpg', 1, 'fotografia', 'Ponte'),
+(2, 'fotografia', 'image', 'img1', 'jpg', NULL, 'fotografia', 'Parado no Movimento'),
+(3, 'fotografia', 'image', 'img2', 'jpg', NULL, 'web development', '&#x3C;CODE&#x3E;'),
+(4, 'fotografia', 'image', 'img3', 'jpg', NULL, 'fotografia', 'MAAT'),
+(5, 'fotografia', 'image', 'img4', 'jpg', NULL, 'fotografia', 'Ponte #2'),
+(6, '3d', 'image', '3d-01', 'jpg', NULL, '3d', 'Mesa Bilhar'),
+(7, '3d', 'image', '3d-02', 'jpg', NULL, '3d', 'Mesa Bilhar #2'),
+(8, '3d', 'image', '3d-03', 'jpg', NULL, '3d', 'Mesa Bilhar #3'),
+(9, 'maasai', 'image', 'maasai-01', 'jpg', NULL, 'Design Gr&#xE1;fico', 'Desenho de logomarca para negócio de joias'),
+(11, 'cinel', 'image', 'cinel-01', 'jpg', NULL, 'Design Gr&#xE1;fico', 'Brochura de Eletrónica'),
+(12, 'cinel', 'image', 'cinel-03', 'jpg', NULL, 'Design Gr&#xE1;fico', 'Pré-visualização de um MUPI'),
+(13, 'cinel', 'image', 'cinel-02', 'jpg', NULL, 'Design Gr&#xE1;fico', 'Pré-visualização de um Outdoor'),
+(14, 'connect', 'image', 'connect-01', 'jpg', NULL, 'web design', 'Website fictício para uma empresa de serviços de comunicação'),
+(15, 'diga', 'image', 'diga-01', 'jpg', NULL, 'Design Gr&#xE1;fico', 'Desenho final do logótipo'),
+(16, 'kiosk', 'image', 'kiosk-01', 'jpg', NULL, 'web development', 'Desenho e desevolvimento de uma plataforma noticiosa'),
+(18, 'unicuidar', 'image', 'unicuidar-01', 'jpg', NULL, 'Design Gr&#xE1;fico', 'Desenho de marca e branding para negócio de serviços de enfermagem'),
+(19, 'unicuidar', 'image', 'unicuidar-02', 'jpg', NULL, 'Design Gr&#xE1;fico', 'Desenhode marca e branding para negócio de serviços de enfermagem'),
+(20, 'unicuidar', 'image', 'unicuidar-03', 'jpg', NULL, 'Design Gr&#xE1;fico', 'Desenhode marca e branding para negócio de serviços de enfermagem'),
+(21, 'car', 'image', 'car-01', 'jpg', NULL, 'Design Gr&#xE1;fico', 'CarExpress');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `projetos`
+-- Estrutura da tabela `projetos`
 --
 
 CREATE TABLE IF NOT EXISTS `projetos` (
   `id_projeto` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(200) COLLATE utf8_bin NOT NULL,
-  `descricao` text COLLATE utf8_bin NOT NULL,
-  `tags` varchar(500) COLLATE utf8_bin NOT NULL,
-  `summary-line` varchar(500) COLLATE utf8_bin NOT NULL,
+  `nome` varchar(200) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `descricao` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `tags` varchar(500) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `summary-line` varchar(500) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `likes` int(10) NOT NULL,
   `views` int(10) NOT NULL,
   `share` int(10) NOT NULL,
-  `uri` varchar(100) COLLATE utf8_bin NOT NULL,
+  `uri` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `external-urls` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id_projeto`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=12 ;
 
 --
--- Dumping data for table `projetos`
+-- Extraindo dados da tabela `projetos`
 --
 
 INSERT INTO `projetos` (`id_projeto`, `nome`, `descricao`, `tags`, `summary-line`, `likes`, `views`, `share`, `uri`, `external-urls`) VALUES
-(1, 'Dummy Project', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum consectetur hendrerit purus, ut volutpat enim posuere a. Donec efficitur quam nec orci aliquam, ac fringilla ipsum vehicula. Praesent sagittis lacinia diam. Suspendisse fringilla varius diam, in rhoncus libero auctor ut. Aliquam sed justo lorem. Vivamus venenatis lacus ligula, faucibus aliquet enim malesuada at. Sed vel eros nunc. Donec erat risus, tincidunt vitae commodo vel, congue eget lectus.<br><br>\nPraesent at nulla lacinia, condimentum diam eget, mattis libero. Nullam condimentum pretium posuere. In in luctus dui. Nullam ornare scelerisque nisl nec varius. Aliquam erat volutpat. Aenean accumsan vulputate maximus. Maecenas quam mauris, mattis at neque vel, imperdiet accumsan dui. Nullam et risus et mi fringilla volutpat at et mi. In gravida cursus massa id fermentum. Donec aliquam arcu nec risus tincidunt viverra. Aenean blandit faucibus condimentum.<br><br>\norem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum consectetur hendrerit purus, ut volutpat enim posuere a. Donec efficitur quam nec orci aliquam, ac fringilla ipsum vehicula. Praesent sagittis lacinia diam. Suspendisse fringilla varius diam, in rhoncus libero auctor ut. Aliquam sed justo lorem. Vivamus venenatis lacus ligula, faucibus aliquet enim malesuada at. Sed vel eros nunc. Donec erat risus, tincidunt vitae commodo vel, congue eget lectus.<br><br>\nPraesent at nulla lacinia, condimentum diam eget, mattis libero. Nullam condimentum pretium posuere. In in luctus dui. Nullam ornare scelerisque nisl nec varius. Aliquam erat volutpat. Aenean accumsan vulputate maximus. Maecenas quam mauris, mattis at neque vel, imperdiet accumsan dui. Nullam et risus et mi fringilla volutpat at et mi. In gravida cursus massa id fermentum. Donec aliquam arcu nec risus tincidunt viverra. Aenean blandit faucibus condimentum.', 'html, css,web-design, web-development', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.', 230, 230, 230, 'dummy-project', 1),
-(2, 'Dummy Project #2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum consectetur hendrerit purus, ut volutpat enim posuere a. Donec efficitur quam nec orci aliquam, ac fringilla ipsum vehicula. Praesent sagittis lacinia diam. Suspendisse fringilla varius diam, in rhoncus libero auctor ut. Aliquam sed justo lorem. Vivamus venenatis lacus ligula, faucibus aliquet enim malesuada at. Sed vel eros nunc. Donec erat risus, tincidunt vitae commodo vel, congue eget lectus.<br><br>\r\nPraesent at nulla lacinia, condimentum diam eget, mattis libero. Nullam condimentum pretium posuere. In in luctus dui. Nullam ornare scelerisque nisl nec varius. Aliquam erat volutpat. Aenean accumsan vulputate maximus. Maecenas quam mauris, mattis at neque vel, imperdiet accumsan dui. Nullam et risus et mi fringilla volutpat at et mi. In gravida cursus massa id fermentum. Donec aliquam arcu nec risus tincidunt viverra. Aenean blandit faucibus condimentum.<br><br>\r\norem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum consectetur hendrerit purus, ut volutpat enim posuere a. Donec efficitur quam nec orci aliquam, ac fringilla ipsum vehicula. Praesent sagittis lacinia diam. Suspendisse fringilla varius diam, in rhoncus libero auctor ut. Aliquam sed justo lorem. Vivamus venenatis lacus ligula, faucibus aliquet enim malesuada at. Sed vel eros nunc. Donec erat risus, tincidunt vitae commodo vel, congue eget lectus.<br><br>\r\nPraesent at nulla lacinia, condimentum diam eget, mattis libero. Nullam condimentum pretium posuere. In in luctus dui. Nullam ornare scelerisque nisl nec varius. Aliquam erat volutpat. Aenean accumsan vulputate maximus. Maecenas quam mauris, mattis at neque vel, imperdiet accumsan dui. Nullam et risus et mi fringilla volutpat at et mi. In gravida cursus massa id fermentum. Donec aliquam arcu nec risus tincidunt viverra. Aenean blandit faucibus condimentum.', 'design-grafico, branding, web-development', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.', 230, 230, 230, 'dummy-project2', 1);
+(3, 'fotografia', 'Algumas fotos que tirei', 'fotografia', 'Algumas fotos que tirei', 0, 0, 0, 'fotografias', NULL),
+(4, 'Modelação de Mesa de Bilhar', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '3d', 'Modelação de mesa de bilhar com bolas texturizadas', 0, 0, 0, '3d', NULL),
+(5, 'Comunicação externa para a CINEL', 'dadasd', 'design-gr%C3%A1fico', 'Criação de comunicação institucional', 0, 0, 0, 'cinel', NULL),
+(6, 'Connect Website', 'djashjjsakladf', 'web-design, html, css', 'Site ficticio sobre uma empresa de comunicações', 0, 0, 0, 'connect', NULL),
+(7, 'Diga que vai daqui', 'asdfbfasd', 'design-gráfico, logomarca, estacion%C3%A1rio', 'Logomarca para página sobre viagens', 0, 0, 0, 'diga', NULL),
+(8, 'Kiosk Digital', 'sdafjdajkdhakjsdhakjsdhaksjdhakjsd', 'web-development,css,html,php, web-design', 'A tua porta para a internet', 0, 0, 0, 'kiosk', NULL),
+(9, 'Maasai', 'adjkajdaksldjaksdj', 'design-gr%C3%A1fico, logomarca, estacion%C3%A1rio', 'Empresa de joias baseada em tribo aficana', 0, 0, 0, 'maasai', NULL),
+(10, 'UniCuidar', 'adaklsdjaljdaksdj', 'design-gr%C3%A1fico, logomarca,estacion%C3%A1rio', 'Serviços de enfermagem personalizados', 0, 0, 0, 'unicuidar', NULL),
+(11, 'CarExpress', 'asdasdasdjl', 'design-gr%C3%A1fico, logomarca', 'Marca ficticia de oficina automóvel', 0, 0, 0, 'car', NULL);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
