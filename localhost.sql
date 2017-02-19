67ac9d9a5b84e59517c5a384dbdaa263936d5b10
Alice <alicescfernandes@gmail.com>
2017-02-19 23:53:59 +0000

    added sql

--- localhost.sql
+++ localhost.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Máquina: localhost
--- Data de Criação: 16-Fev-2017 às 22:53
+-- Data de Criação: 19-Fev-2017 às 23:47
 -- Versão do servidor: 5.6.13
 -- versão do PHP: 5.4.17
 
@@ -58,10 +58,10 @@ CREATE TABLE IF NOT EXISTS `media` (
   `src` varchar(700) COLLATE utf8_bin NOT NULL,
   `ext` varchar(50) COLLATE utf8_bin NOT NULL,
   `cover` tinyint(1) DEFAULT NULL,
-  `title` varchar(1000) COLLATE utf8_bin NOT NULL,
+  `title` varchar(1000) COLLATE utf8_bin DEFAULT NULL,
   `nome` varchar(100) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id_media`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=22 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=26 ;
 
 --
 -- Extraindo dados da tabela `media`
@@ -69,24 +69,27 @@ CREATE TABLE IF NOT EXISTS `media` (
 
 INSERT INTO `media` (`id_media`, `projeto_uri`, `tipo`, `src`, `ext`, `cover`, `title`, `nome`) VALUES
 (1, 'fotografia', 'image', 'img', 'jpg', 1, 'fotografia', 'Ponte'),
-(2, 'fotografia', 'image', 'img1', 'jpg', NULL, 'fotografia', 'Parado no Movimento'),
-(3, 'fotografia', 'image', 'img2', 'jpg', NULL, 'web development', '&#x3C;CODE&#x3E;'),
-(4, 'fotografia', 'image', 'img3', 'jpg', NULL, 'fotografia', 'MAAT'),
-(5, 'fotografia', 'image', 'img4', 'jpg', NULL, 'fotografia', 'Ponte #2'),
-(6, '3d', 'image', '3d-01', 'jpg', NULL, '3d', 'Mesa Bilhar'),
-(7, '3d', 'image', '3d-02', 'jpg', NULL, '3d', 'Mesa Bilhar #2'),
-(8, '3d', 'image', '3d-03', 'jpg', NULL, '3d', 'Mesa Bilhar #3'),
-(9, 'maasai', 'image', 'maasai-01', 'jpg', NULL, 'Design Gr&#xE1;fico', 'Desenho de logomarca para negócio de joias'),
-(11, 'cinel', 'image', 'cinel-01', 'jpg', NULL, 'Design Gr&#xE1;fico', 'Brochura de Eletrónica'),
-(12, 'cinel', 'image', 'cinel-03', 'jpg', NULL, 'Design Gr&#xE1;fico', 'Pré-visualização de um MUPI'),
-(13, 'cinel', 'image', 'cinel-02', 'jpg', NULL, 'Design Gr&#xE1;fico', 'Pré-visualização de um Outdoor'),
-(14, 'connect', 'image', 'connect-01', 'jpg', NULL, 'web design', 'Website fictício para uma empresa de serviços de comunicação'),
-(15, 'diga', 'image', 'diga-01', 'jpg', NULL, 'Design Gr&#xE1;fico', 'Desenho final do logótipo'),
-(16, 'kiosk', 'image', 'kiosk-01', 'jpg', NULL, 'web development', 'Desenho e desevolvimento de uma plataforma noticiosa'),
-(18, 'unicuidar', 'image', 'unicuidar-01', 'jpg', NULL, 'Design Gr&#xE1;fico', 'Desenho de marca e branding para negócio de serviços de enfermagem'),
-(19, 'unicuidar', 'image', 'unicuidar-02', 'jpg', NULL, 'Design Gr&#xE1;fico', 'Desenhode marca e branding para negócio de serviços de enfermagem'),
-(20, 'unicuidar', 'image', 'unicuidar-03', 'jpg', NULL, 'Design Gr&#xE1;fico', 'Desenhode marca e branding para negócio de serviços de enfermagem'),
-(21, 'car', 'image', 'car-01', 'jpg', NULL, 'Design Gr&#xE1;fico', 'CarExpress');
+(2, 'fotografia', 'image', 'img1', 'jpg', 1, 'fotografia', 'Parado no Movimento'),
+(3, '', 'image', 'img2', 'jpg', 1, 'web development', '&#x3C;CODE&#x3E;'),
+(4, 'fotografia', 'image', 'img3', 'jpg', 1, 'fotografia', 'MAAT'),
+(5, 'fotografia', 'image', 'img4', 'jpg', 1, 'fotografia', 'Ponte #2'),
+(6, '3d', 'image', '3d-01', 'jpg', 1, '3d', 'Mesa Bilhar'),
+(7, '3d', 'image', '3d-02', 'jpg', 1, '3d', 'Mesa Bilhar #2'),
+(8, '3d', 'image', '3d-03', 'jpg', 1, '3d', 'Mesa Bilhar #3'),
+(9, 'maasai', 'image', 'maasai-01', 'jpg', 1, 'Design Gr&#xE1;fico', 'Desenho de logomarca para negócio de joias'),
+(11, 'cinel', 'image', 'cinel-01', 'jpg', 1, 'Design Gr&#xE1;fico', 'Brochura de Eletrónica'),
+(12, 'cinel', 'image', 'cinel-03', 'jpg', 1, 'Design Gr&#xE1;fico', 'Pré-visualização de um MUPI'),
+(13, 'cinel', 'image', 'cinel-02', 'jpg', 1, 'Design Gr&#xE1;fico', 'Pré-visualização de um Outdoor'),
+(14, 'connect', 'image', 'connect-01', 'jpg', 1, NULL, 'Website fictício para uma empresa de serviços de comunicação'),
+(15, 'diga', 'image', 'diga-01', 'jpg', 1, 'Design Gr&#xE1;fico', 'Desenho final do logótipo'),
+(16, 'kiosk', 'image', 'kiosk-01', 'jpg', 1, 'web design', 'Desenho e desevolvimento de uma plataforma noticiosa'),
+(18, 'unicuidar', 'image', 'unicuidar-01', 'jpg', 1, 'Design Gr&#xE1;fico', 'Desenho de marca e branding para negócio de serviços de enfermagem'),
+(19, 'unicuidar', 'image', 'unicuidar-02', 'jpg', 1, 'Design Gr&#xE1;fico', 'Desenhode marca e branding para negócio de serviços de enfermagem'),
+(20, 'unicuidar', 'image', 'unicuidar-03', 'jpg', 1, 'Design Gr&#xE1;fico', 'Desenhode marca e branding para negócio de serviços de enfermagem'),
+(21, 'car', 'image', 'car-01', 'jpg', 1, 'Design Gr&#xE1;fico', 'CarExpress'),
+(22, 'animacao', 'image', 'rocket_exportacao', 'gif', NULL, NULL, 'Animação Rocket'),
+(24, 'animacao', 'image', 'retro', 'gif', NULL, NULL, 'Animação Retro'),
+(25, 'animacao', 'image', 'rocket_exportacao', 'gif', NULL, NULL, 'Animação Rocket');
 
 -- --------------------------------------------------------
 
@@ -113,7 +116,6 @@ CREATE TABLE IF NOT EXISTS `projetos` (
 --
 
 INSERT INTO `projetos` (`id_projeto`, `nome`, `descricao`, `tags`, `summary-line`, `likes`, `views`, `share`, `uri`, `external-urls`) VALUES
-(3, 'fotografia', 'Algumas fotos que tirei', 'fotografia', 'Algumas fotos que tirei', 0, 0, 0, 'fotografias', NULL),
 (4, 'Modelação de Mesa de Bilhar', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '3d', 'Modelação de mesa de bilhar com bolas texturizadas', 0, 0, 0, '3d', NULL),
 (5, 'Comunicação externa para a CINEL', 'dadasd', 'design-gr%C3%A1fico', 'Criação de comunicação institucional', 0, 0, 0, 'cinel', NULL),
 (6, 'Connect Website', 'djashjjsakladf', 'web-design, html, css', 'Site ficticio sobre uma empresa de comunicações', 0, 0, 0, 'connect', NULL),
