5564f37bb892239cef1ebac1673218cf378621e7
alicescfernandes <1996fernandes@gmail.com>
2017-02-24 15:37:56 +0000

    Comentado os slides
    - TODO: ADD SWIPE VANILLA

--- script/script.js
+++ script/script.js
@@ -3,32 +3,39 @@ function clickFunction() {
 }
 window.onload = function() {
     document.querySelector(".audio").onclick = function(){
-    document.getElementsByTagName("audio")[0].paused ? document.querySelector(".play").classList.remove("icon-controller-play") & document.querySelector(".play").classList.add("icon-controller-paus") & document.getElementsByTagName("audio")[0].play() : document.getElementsByTagName("audio")[0].pause() & document.querySelector(".play").classList.remove("icon-controller-paus") & document.querySelector(".play").classList.add("icon-controller-play");
+      document.getElementsByTagName("audio")[0].paused ? document.querySelector(".play").classList.remove("icon-controller-play") & document.querySelector(".play").classList.add("icon-controller-paus") & document.getElementsByTagName("audio")[0].play() : document.getElementsByTagName("audio")[0].pause() & document.querySelector(".play").classList.remove("icon-controller-paus") & document.querySelector(".play").classList.add("icon-controller-play");
     }
 
-    var currentSlide = 1;
 
-    var slideContainer = document.querySelector('.slide-container');
-    var screenW = window.screen.availWidth;
-    var slides = document.getElementsByClassName('slide') || null;
-    if(document.querySelector('li[data-slide="' + currentSlide + '"]')) document.querySelector('li[data-slide="' + currentSlide + '"]').classList.add("active")
-    var slideDuration = 6 //6 segundos
+document.body.addEventListener('touchstart', function(event){console.log(event)})
+document.body.addEventListener('touchend', function(event){console.log(event)})
 
-    slideContainer.style.width = screenW * slides.length + "px";
+    /*
+    INICIO DE CÓDIGO DE SLIDES
+    */
+
+    var currentSlide = 1; //Define o primeiro slide como o slide actual
+    var slideContainer = document.querySelector('.slide-container'); //Seleciona a div.slide-container que contem todos os slides
+    var screenW = window.screen.availWidth; //Guarda o tamanho factual do ecrã, para fazer calculos de dimensões de imagens
+    var slides = document.getElementsByClassName('slide') || null; //Este operador tenário define a variavel como o conjuto dos slides, ou se os slides não exisiterm, nulifica a variável
+    if(document.querySelector('li[data-slide="' + currentSlide + '"]')) document.querySelector('li[data-slide="' + currentSlide + '"]').classList.add("active") //Seleciomna o primeiro slide e aplica-lhe a classe active
+    var slideDuration = 6 //6 segundos //duração em segundos de cada slied
+
+    slideContainer.style.width = screenW * slides.length + "px"; //Define o tamanho do slide para o tamanho do ecrã
     for(var i = 0; i!= slides.length;i++){
-      slides[i].style.width = screenW + "px";
+      slides[i].style.width = screenW + "px"; //Define o tamanho de cada slide para o tamanho do ecrã
     }
-    var intervalo = window.setInterval(function() {
-
+    var intervalo = window.setInterval(function() { //Função que executa a cada X segundos.
+      //Aumenta o numero do slide. Se o numero do slide corresponder ao último index da array, reseta o numero para 1;
         if (currentSlide <= slides.length - 1) {
             currentSlide++
         } else {
             currentSlide = 1
         }
 
-        var currentSlideDOM = document.querySelector('.slide[data-slide="' + currentSlide + '"]');
+        var currentSlideDOM = document.querySelector('.slide[data-slide="' + currentSlide + '"]'); //Seleciona o slide com base na variavel currentSlide . É neste slide que acontece todas as modificações.
 
-        if (document.getElementsByClassName("active")) {
+        if (document.getElementsByClassName("active")) { //Este bloco serve para remover todas os list items que estão ativos
             var slideControls = document.getElementsByClassName('slider-control');
             for (var element = 0; element != slideControls.length; element++) {
                 if (slideControls[element].classList.contains("active")) {
@@ -37,13 +44,18 @@ window.onload = function() {
             }
         }
 
-        document.querySelector('li[data-slide="' + currentSlide + '"]').classList.add("active")
-        slideContainer.style.left = (currentSlideDOM.offsetLeft * -1) + "px"
+        document.querySelector('li[data-slide="' + currentSlide + '"]').classList.add("active") //Seleciona o list-item corresponnte ao slide aplica a classe active
+        slideContainer.style.left = (currentSlideDOM.offsetLeft * -1) + "px" //Fa< a transição de slides através do da posição left a multiplicar por -1
     }, slideDuration * 1000);
 
+
+/* Este bloco trata dos cliques nos slide-controllers */
     document.body.onclick = function(event) {
+      //Monitoriza todos os cliques
         if (event.target.className == "slider-control") {
+          //Se o target do clique tiver a classe .slider-control
             if (document.getElementsByClassName("active")) {
+              //Se houver .slider-control.active, apaga todos os .actives dos slider-control
                 var slideControls = document.getElementsByClassName('slider-control');
                 for (var element = 0; element != slideControls.length; element++) {
                     if (slideControls[element].classList.contains("active")) {
@@ -51,23 +63,14 @@ window.onload = function() {
                     }
                 }
             }
-            document.querySelector('li[data-slide="' + event.target.getAttribute('data-slide') + '"]').classList.add("active")
-            var currentWidth = document.querySelector('.slide[data-slide="' + event.target.getAttribute('data-slide') + '"]').offsetLeft * -1
-            document.querySelector('.slide-container').style.left = currentWidth + "px";
-            currentSlide = parseInt(event.target.getAttribute('data-slide'));
+            document.querySelector('li[data-slide="' + event.target.getAttribute('data-slide') + '"]').classList.add("active") //Aplica a classe active ao target através dos data-attributes
+            var currentWidth = document.querySelector('.slide[data-slide="' + event.target.getAttribute('data-slide') + '"]').offsetLeft * -1 //Calcula a proxima posição que o slide-container tem de ter através da data-slide do target aplicado na selecão do .slide através do nt
         }
     }
 
-    /*document.querySelector("#animation").onclick = function() {
-        document.querySelector("#animation > div").classList.add("button-animate");
-        window.setTimeout(function() {
-            document.querySelector("#animation > div").classList.remove("button-animate");
-            location.href = location.href + document.querySelector("#animation").getAttribute('href');
-        }, 150)
-        return false;
-
-    }*/
-
+    /*
+    FIM DE CÓDIGO DE SLIDES
+    */
 }
 
 
--- templates/index.php
+++ templates/index.php
@@ -6,7 +6,7 @@
     require_once('./php/db-constants.php');
     $maxImages = 5;
     $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-    $query = "SELECT * FROM `media` WHERE title IS NOT NULL GROUP BY title ORDER BY RAND() LIMIT $maxImages";
+    $query = "SELECT * FROM `media` WHERE title IS NOT NULL GROUP BY title ORDER BY RAND() LIMIT $maxImages";// EXPLICAÇÃO DE COMPONENTE | Esta query vai selecionar todas a s imagens que estão antabela média. Esta tabela contem o src, title de cada imagem. A função RAND() que é usada com o ORDER BY ordena de maneira aleatória as imagens.
     $result = $conn->query($query);
     $resultHTML = '';
 
@@ -15,28 +15,18 @@
       $string = $row['src'];
       $metaImage = "http://".$host . "/" ."media/".$string."@1280.jpg";
       $description = $row['title'];
+
+      /* EXPLICAÇÃO DE COMPONENTE
+
+      Depois de feita a query à base de dados, o PHP corre pelas linhas do result set e gera o html necessário para os slides.
+      A imagem do slide é colocada como fundo, porque asism fica mais fácil redimensionar a imagem, e porque posso manipular a posição e o tamanho da imagem como uma mascara
+
+      */
+
       $resultHTML.="<div class=\"slide\" style=\"background-image:url(http://".$host . "/" ."media/".$string."@1900.jpg)\" data-slide=\"".($i+1)."\"><div class=\"title\"><p>$description</p></div></div>";
       $i++;
     }
-    //$randImages = [];
-    //$randImagesDescription = [];
-    /*
-    while($array = $result->fetch_object()){
-      $randImages[$i] = $array->src;
-      $randImagesDescription[$i] = $array->title;
-      $i++;
-    }
 
-   $randArray = range(0,count($randImages)-1);
-   shuffle($randArray);
-   $randArray = array_slice($randArray, 0, $maxImages);
-
-    for($i = 0; $i!=$maxImages;$i++){
-      $string = $randImages[$randArray[$i]];
-      $metaImage = "http://".$host . "/" ."media/".$string."@1280.jpg";
-      $description = $randImagesDescription[$randArray[$i]];
-      $resultHTML.="<div class=\"slide\" style=\"background-image:url(http://".$host . "/" ."media/".$string."@1900.jpg)\" data-slide=\"".($i+1)."\"><div class=\"title\"><p>$description</p></div></div>";
-    }*/
 
      ?>
 <title>Página Inicial | Alice Fernandes - Web Development & Design</title>
@@ -64,10 +54,20 @@
             <p>Design & Development</p></span></header>
         <div class="ui-component slideshow slideshow-fullpage slideshow-absolute-pos">
           <div class="slide-container">
+            <!-- EXPLICAÇÃO DE COMPONENTE
+            É nesta div que é colocado o conteúdo gerado com PHP.
+            -->
             <?php echo($resultHTML); ?>
             </div>
           <div class="ui-component slide-controller">
             <ul>
+            <!-- EXPLICAÇÃO DE COMPONENTE
+
+            Cada elemento da lista corresponde a uma imagem no slide.
+            As imagens são retiradas da base de dados aleatóriamente, mas o numero de slides já está pré -estabelecido.
+            Para colocar mais slides, basta aumentar o numero de itens e colocar mais titles na tabela media.
+
+            -->
               <li class="slider-control" data-slide="1"> </li>
               <li class="slider-control" data-slide="2"></li>
               <li class="slider-control" data-slide="3"></li>
